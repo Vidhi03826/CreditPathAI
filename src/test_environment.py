407dc97bf76146efb85a580de373bf841b7877ab
Vidhi03826 <vidh312@gmail.com>
2026-05-23 18:37:46 +0530

    Day 1 project setup completed

---
+++ src/test_environment.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+print("Pandas version:", pd.__version__)
+print("NumPy version:", np.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+
+print("Environment setup successful!")
